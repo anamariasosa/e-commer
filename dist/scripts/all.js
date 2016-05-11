4886f22cf0e315e18d8be2335d899e08b384ecb9
anamariasosa <anamariasosam@gmail.com>
2016-05-11 18:25:56 -0500

    delete dist folder

--- dist/scripts/all.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-	// Este evento activa las tabs del modal
-	$('.product__menu_tabs a').on('click', function (e) {
-	  e.preventDefault()
-	  $(this).tab('show')
-	});
-});
