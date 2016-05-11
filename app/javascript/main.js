06a7850b57a8a6194a02899e912721507e7dbcbb
anamariasosa <anamariasosam@gmail.com>
2016-05-11 18:04:12 -0500

    [improve]

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function($) {
-	$('.product__menu_tabs a').click(function (e) {
+	// Este evento activa las tabs del modal
+	$('.product__menu_tabs a').on('click', function (e) {
 	  e.preventDefault()
 	  $(this).tab('show')
-	})
+	});
 });
